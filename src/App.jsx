456fa0ab6c639251ca47905877615fd0acc46966
Luigi Gandemer <luigigandemer6@gmail.com>
2026-02-15 16:49:57 +0100

    Add error message

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ function App() {
         duration: 10
     });
 
+    const inputIsValid = userInput.duration >= 1;
+
     function handleChange(inputIdentifier, newValue) {
         setUserInput(prevUserInput => {
             return {
@@ -26,7 +28,8 @@ function App() {
         <>
             <Header />
             <UserInput userInput={userInput} handleChange={handleChange} />
-            <Results input={userInput} />
+            {inputIsValid && <Results input={userInput} /> }
+            {!inputIsValid && <p className='center'>Please enter duration greate then 0.</p> }
         </>
     )
 }
